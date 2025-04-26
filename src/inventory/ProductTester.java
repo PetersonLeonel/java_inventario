6ec60059dfe11858add891f90631921935c4eec6
PetersonLeonel <peter.mlalves@gmail.com>
2025-04-26 16:59:51 -0300

    Fatoração

--- src/inventory/ProductTester.java
+++ src/inventory/ProductTester.java
@@ -6,12 +6,54 @@ public class ProductTester {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		int maxSize;
+		maxSize = getNumProducts(in);
+				
+		if(maxSize == 0) {
+			System.out.println("Nenhum produto requerido para ser registrado!");
+		} else {
+			
+			Product[] products = new Product[maxSize];
+			addToInventory(products, in);
+			displayInventory(products);
+		}
+		
+		in.close();
+	}
+	
+	static void displayInventory(Product[] products) {
+		// Exibe os produtos armazenados
+		for(Product product: products) {
+			System.out.println(product);
+		}
+	}
+	
+	static void addToInventory(Product[] products, Scanner in) {
 		int tempNumber;
 		String tempName;
 		int tempQtd;
 		double tempPrice;
+		
+		// Armazenar produtos
+		for(int i = 0; i < products.length; i++) {
+			in.nextLine();
+			System.out.println("\n");
+			System.out.println("Digite o nome do produto: ");
+			tempName = in.nextLine();
+			System.out.println("Digite a quantidade do estoque: ");
+			tempQtd = in.nextInt();
+			System.out.println("Digite o preço: ");
+			tempPrice = in.nextDouble();
+			System.out.println("Digite o identificador único: ");
+			tempNumber = in.nextInt();
+			// criar um objeto produto e armazenar suas propriedades
+			products[i] = new Product(tempNumber, tempName, tempQtd, tempPrice);
+		}
+					
+	}
+	
+	static int getNumProducts(Scanner in) {
 		int maxSize = -1;
-				
 		// Avisar o usuário até que ele insira um número >= 0
 		do {
 			try {
@@ -28,36 +70,6 @@ public class ProductTester {
 			}
 		} while(maxSize<0);
 		
-		if(maxSize == 0) {
-			System.out.println("Nenhum produto requerido para ser registrado!");
-		} else {
-			// Armazenar produtos
-			Product[] products = new Product[maxSize];
-			for(int i = 0; i < maxSize; i++) {
-				in.nextLine();
-				System.out.println("\n");
-				System.out.println("Digite o nome do produto: ");
-				tempName = in.nextLine();
-				System.out.println("Digite a quantidade do estoque: ");
-				tempQtd = in.nextInt();
-				System.out.println("Digite o preço: ");
-				tempPrice = in.nextDouble();
-				System.out.println("Digite o identificador único: ");
-				tempNumber = in.nextInt();
-				// criar um objeto produto e armazenar suas propriedades
-				products[i] = new Product(tempNumber, tempName, tempQtd, tempPrice);
-			}
-			
-			// Exibe os produtos armazenados
-			for(Product product: products) {
-				System.out.println(product);
-			}
-		}
-		
-		
-		
-		
-		in.close();
+		return maxSize;
 	}
-
 }
